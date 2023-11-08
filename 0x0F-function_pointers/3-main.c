391924bc9d6b9b8fed82b6eb1d0978b843d34488
Teespecific <tokwe58@gmail.com>
2023-11-08 15:09:00 -0800

    struct op

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: Alaways 0.
  */
-int main(int _attribute_((_unused_)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
